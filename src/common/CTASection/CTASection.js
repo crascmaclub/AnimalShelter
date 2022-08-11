fd237374728a79cc7f47ed4e273c29f986c44f10
Supporter09 <maivannhatminh2005@gmail.com>
2022-08-11 20:52:23 +0700

    fix cta section

--- src/common/CTASection/CTASection.js
+++ src/common/CTASection/CTASection.js
@@ -105,7 +105,7 @@ export const CTASection = () => {
                                 borderRadius: '0px 5px 5px 0px',
                             })}
                         >
-                            Search
+                            Tham Gia
                         </Button>
                     </Box>
                 </Box>
